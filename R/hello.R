58e4a5e8a6127a6e5f3217621636af62f2582f28
LinMi <15527504293@163.com>
2022-04-08 01:48:36 +0800

    第一次上传

---
+++ R/hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/liamamilin/ddsa.git
+git push -u origin main
